d6fa2c2d6a128fce214f17ec23be0215ead0d69e
DerekSun <77709066+DerekSun02@users.noreply.github.com>
2026-08-17 14:38:32 -0400

    LC-kth-smallest-element-in-a-sorted-matrix [Runtime: N/A; Memory: N/A]

--- array/kth-smallest-element-in-a-sorted-matrix.java
+++ array/kth-smallest-element-in-a-sorted-matrix.java
@@ -6,24 +6,22 @@ class Solution {
             // 按照元素大小升序
             return a[0] - b[0]; 
         });
-        int count = 0; 
         for(int i = 0; i < m; i++){
             minHeap.offer(new int[]{matrix[i][0], i, 0});  
         }
-        while(!minHeap.isEmpty()){
+        int res = -1; 
+        while(!minHeap.isEmpty() && k > 0){
             // poll最小元素
             int[] polled = minHeap.poll(); 
             int number = polled[0], i = polled[1], j = polled[2];
-            count++; 
-            // 判断是否是第k个最小元素 
-            if (count == k){
-                return number; 
-            }
+            res = number; 
+            k--; 
+            
             // 将下一个元素matrix[i][j+1]加入队列
             if (matrix[i].length > j+1){
                 minHeap.offer(new int[]{matrix[i][j+1], i, j+1}); 
             }
         }
-        return 0; 
+        return res; 
     }
 }
